94dd4af23b8edf618873183b9d2617e12b48a581
Buratud Thumboon <62024314+buratud@users.noreply.github.com>
2023-10-01 16:09:53 +0700

    Use `SerializedName` from gson

--- src/main/java/com/buratud/data/openai/chat/ChatCompletionResponse.java
+++ src/main/java/com/buratud/data/openai/chat/ChatCompletionResponse.java
@@ -2,29 +2,29 @@ package com.buratud.data.openai.chat;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 public class ChatCompletionResponse {
     @Expose
-    @JsonProperty("id")
+    @SerializedName("id")
     public String id;
     @Expose
-    @JsonProperty("object")
+    @SerializedName("object")
     public String object;
     @Expose
-    @JsonProperty("created")
+    @SerializedName("created")
     public long created;
     @Expose
-    @JsonProperty("model")
+    @SerializedName("model")
     public String model;
     @Expose
-    @JsonProperty("choices")
+    @SerializedName("choices")
     public List<Choice> choices;
     @Expose
-    @JsonProperty("usage")
+    @SerializedName("usage")
     public Usage usage;
 
     public static ChatCompletionResponse fromJson(String json) {
