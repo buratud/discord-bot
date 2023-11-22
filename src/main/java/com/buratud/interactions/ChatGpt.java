1d49677478ad117d8aa218f0e3105e3699f957fe
Buratud Thumboon <buratud@gmail.com>
2023-11-22 23:15:32 +0700

    Bugfix: null object on first request

--- src/main/java/com/buratud/interactions/ChatGpt.java
+++ src/main/java/com/buratud/interactions/ChatGpt.java
@@ -49,7 +49,7 @@ public final class ChatGpt implements Handler {
                 String channelId = event.getChannel().getId();
                 String userId = event.getAuthor().getId();
                 ChatGptChannelInfo info = chatGpt.getInfo(channelId, userId);
-                if (message.getMentions().isMentioned(event.getJDA().getSelfUser()) || info.activated) {
+                if (message.getMentions().isMentioned(event.getJDA().getSelfUser()) || info != null && info.activated) {
                     if (message.getMentions().isMentioned(event.getJDA().getSelfUser())) {
                         int pos = rawMessage.indexOf(String.format("<@%s>", event.getJDA().getSelfUser().getId()));
                         int lastPos = rawMessage.indexOf('>', pos);
