daa6750a12bea01125013ccae22ddf5423f7e823
Buratud Thumboon <buratud@gmail.com>
2023-12-20 10:29:22 +0700

    feat: Explicitly specify serialized name for dynamodb

--- src/main/java/com/buratud/data/ChannelData.java
+++ src/main/java/com/buratud/data/ChannelData.java
@@ -3,10 +3,16 @@ package com.buratud.data;
 import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
 
 @Getter
 @Setter
 public class ChannelData extends GuildData {
     @SerializedName("channel_id")
     protected String channelId;
+
+    @DynamoDbAttribute("channel_id")
+    public String getChannelId() {
+        return channelId;
+    }
 }
--- src/main/java/com/buratud/data/GuildData.java
+++ src/main/java/com/buratud/data/GuildData.java
@@ -3,10 +3,16 @@ package com.buratud.data;
 import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
 
 @Getter
 @Setter
 public class GuildData {
     @SerializedName("guild_id")
     protected String guildId;
+
+    @DynamoDbAttribute("guild_id")
+    public String getGuildId() {
+        return guildId;
+    }
 }
