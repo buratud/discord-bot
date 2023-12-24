3965331b7c33c0436d31de6b299dc57e3081510b
Buratud Thumboon <buratud@gmail.com>
2023-12-24 20:44:38 +0700

    bugfix: Add JsonIgnoreProperties on ocr entity

--- src/main/java/com/buratud/data/azure/computervision/ReadResponseBody.java
+++ src/main/java/com/buratud/data/azure/computervision/ReadResponseBody.java
@@ -1,10 +1,15 @@
 package com.buratud.data.azure.computervision;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ReadResponseBody {
     public String status;
     public AnalyzeResult analyzeResult;
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class AnalyzeResult {
         public ReadResult[] readResults;
+        @JsonIgnoreProperties(ignoreUnknown = true)
         public static class ReadResult {
             public Line[] lines;
             public static class Line {
